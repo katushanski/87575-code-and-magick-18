43054161569d1d666876476c335c3bb93e6130d0
katushanski <katyadrobot@gmail.com>
2019-09-15 00:04:04 +0300

    Частично исправила замечания линтера

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 var fireballSize = 22;
 var getFireballSpeed = function (left) {
-	if (left = true) {
-		return 5;
-	}
+  if (left = true) {
+  	return 5;
+  }
 
-	return 2;
+  return 2;
 }; // Маша, как рефакторить эту часть? Можно ли написать left ? 5 : 2; ??
 
 wizardSpeed = 3;
 wizardWidth = 70;
 getWizardHeight = function () {
-	var wizardHeight = 1.337 * wizardWidth;
-	return wizardHeight;
+  var wizardHeight = 1.337 * wizardWidth;
+  return wizardHeight;
 };
 
 getWizardX = function (width) {
-	return (width-wizardWidth) / 2
+  return (width-wizardWidth) / 2
 };
 
 getWizardY = function (height) {
-	return height / 3;
+  return height / 3;
 };
