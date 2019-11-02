8776d71db5d25d1867e51dd5a536a9d147675e4e
katushanski <katyadrobot@gmail.com>
2019-11-02 18:06:58 +0200

    Начала настройку смены цвета элементов по клику

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,136 @@
 'use strict';
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var userDialogElement = document.querySelector('.setup');
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+// Открытие/закрытие окна настройки персонажа:
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  userDialogElement.classList.remove('hidden');
+};
+
+var closePopup = function () {
+  userDialogElement.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var openDialogElement = document.querySelector('.setup-open');
+openDialogElement.addEventListener('click', function () {
+  openPopup();
+  document.addEventListener('keydown', onPopupEscPress);
+});
+
+var openDialogIcon = openDialogElement.querySelector('.setup-open-icon');
+openDialogIcon.addEventListener('focus', function () { // Маша, можно ли так записать? логика есть? я подсмотрела уже в демке, что можно проще, но поскольку изначально писала самостоятельно, чтобы разобраться, теперь интересно, можно ли так написать.
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      openPopup();
+    }
+  });
+});
+
+var closeDialogElement = userDialogElement.querySelector('.setup-close');
+closeDialogElement.addEventListener('click', function () {
+  closePopup();
+});
+
+var setupForm = userDialogElement.querySelector('.setup-wizard-form');
+var usernameInput = setupForm.querySelector('.setup-user-name');
+usernameInput.addEventListener('focus', function () {
+  document.removeEventListener('keydown', onPopupEscPress);
+});
+
+var formSubmitButton = setupForm.querySelector('.setup-submit');
+formSubmitButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  setupForm.submit();
+});
+
+formSubmitButton.addEventListener('focus', function (evt) {
+  evt.preventDefault();
+  document.addEventListener('keydown', function (event) {
+    if (event.keyCode === ENTER_KEYCODE) {
+      setupForm.submit();
+    }
+  });
+});
+
+// Валидация ввода имени персонажа (взято из демки)
+usernameInput.addEventListener('invalid', function (evt) {
+  evt.preventDefault();
+  if (usernameInput.validity.tooShort) {
+    usernameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (usernameInput.validity.tooLong) {
+    usernameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (usernameInput.validity.valueMissing) {
+    usernameInput.setCustomValidity('Обязательное поле');
+  } else {
+    usernameInput.setCustomValidity('');
+  }
+});
+
+// Кастомизация персонажа
+var myWizardParams = {
+  COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
+  FIREBALL_COLORS: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
+};
+
+// Изменение цвета мантии персонажа по нажатию
+var setupPlayer = document.querySelector('.setup-player');
+var setupMyWizard = setupPlayer.querySelector('.setup-wizard');
+var coatColorMyWizard = setupMyWizard.querySelector('.wizard-coat');
+var eyesColorMyWizard = setupMyWizard.querySelector('.wizard-eyes');
+var fireballColorMyWizard = setupPlayer.querySelector('.setup-fireball-wrap');
+
+var currentCoatIndex = 0;
+var currentEyesIndex = 0;
+var currentFireballIndex = 0;
+
+var getWizardParam = function (colors, index) {
+  index += 1;
+  return colors[index];
+};
+
+coatColorMyWizard.addEventListener('click', function () {
+  coatColorMyWizard.style.fill = getWizardParam(myWizardParams.COAT_COLORS, currentCoatIndex);
+  setupPlayer.querySelector('input[name="coat-color"]').value = coatColorMyWizard.style.fill;
+  if (currentCoatIndex <= myWizardParams.COAT_COLORS.length) {
+    currentCoatIndex++;
+  } else if (currentCoatIndex > myWizardParams.COAT_COLORS.length) {
+    currentCoatIndex = 0;
+  }
+});
+
+eyesColorMyWizard.addEventListener('click', function () {
+  eyesColorMyWizard.style.fill = getWizardParam(myWizardParams.EYES_COLORS, currentEyesIndex);
+  setupPlayer.querySelector('input[name="eyes-color"]').value = eyesColorMyWizard.style.fill;
+  if (currentEyesIndex <= myWizardParams.EYES_COLORS.length) {
+    currentEyesIndex++;
+  } else if (currentEyesIndex > myWizardParams.EYES_COLORS.length) {
+    currentEyesIndex = 0;
+  }
+});
+
+fireballColorMyWizard.addEventListener('click', function () {
+  fireballColorMyWizard.style.backgroundColor = getWizardParam(myWizardParams.FIREBALL_COLORS, currentFireballIndex);
+  fireballColorMyWizard.querySelector('input[name="fireball-color"]').value = fireballColorMyWizard.style.backgroundColor;
+  if (currentFireballIndex <= myWizardParams.FIREBALL_COLORS.length) {
+    currentFireballIndex++;
+  } else if (currentFireballIndex > myWizardParams.FIREBALL_COLORS.length) {
+    currentFireballIndex = 0;
+  }
+});
+
+// Создание армии магов
+
+userDialogElement.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template') // нахожу шаблон, который буду копировать
